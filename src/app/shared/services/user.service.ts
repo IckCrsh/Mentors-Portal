faec6ecf71189f17d2cc72ddd5c98c6fe1d9729d
Ihor Chernysh <102924490+IckCrsh@users.noreply.github.com>
2022-08-05 13:00:36 +0300

    Added service

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+}
+
+// Создать три метода
+
+// 1. Добавить данные в локалсторадж
+
+// 2. Брать дани з локалсторж
+
+// 3. Сравнивать данные
