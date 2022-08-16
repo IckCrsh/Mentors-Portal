99138d86c7b1dd5cc37424f3828398156213d184
Ihor Chernysh <102924490+IckCrsh@users.noreply.github.com>
2022-08-16 14:27:53 +0300

    added setter, getter.

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { ILogin } from "../interfaces/login.interface";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-constructor(private http: HttpClient){}
+  private _currentUser = new BehaviorSubject(null);
 
-login(value: ILogin) {
- // @ts-ignore
-  return this.http.post('https://mentors-portal.herokuapp.com/auth/login', {
-    username: value.username,
-    password: value.password
-  } )
+  constructor(private http: HttpClient) {
+  }
+
+  login(value: ILogin) {
+    // @ts-ignore
+    return this.http.post('https://mentors-portal.herokuapp.com/auth/login', {
+      username: value.username,
+      password: value.password
+    })
 
 }
 registration(value: ILogin) {
