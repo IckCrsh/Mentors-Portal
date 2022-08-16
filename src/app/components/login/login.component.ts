c589ec342c64e211a19364f296fdc6d39e6c405a
Ihor Chernysh <102924490+IckCrsh@users.noreply.github.com>
2022-08-16 14:27:53 +0300

    added setter, getter.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../shared/services/user.service";
-import { switchMap } from "rxjs";
+import { switchMap, tap, toArray } from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -9,25 +9,32 @@ import { switchMap } from "rxjs";
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit(): void {
   }
-submit() {
+
+  submit() {
     // TODO Удалить, коли створим логин компонент
-   this.userService.login({
+    this.userService.login({
       username: 'ickizz@gmail.com',
       password: '!Ickizz17'
     })
-     .pipe(
-       switchMap(item => {
-         // @ts-ignore
-         return this.userService.getUsers(item.token)
-       }),
-     )
-     .subscribe( res => {
-     console.log(res);
-   });
-  // localStorage.clear()
-}
+      .pipe(
+        switchMap(item => {
+          // @ts-ignore
+          return this.userService.getUsers(item.token)
+        }),
+        tap(item => {
+          // @ts-ignore
+          const currentUser = item.find(element => element.username == 'ickizz@gmail.com')
+          this.userService.currentUser = currentUser;
+        })
+      )
+      .subscribe(res => {
+
+        console.log(res);
+      });
+  }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,33 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { ILogin } from "../interfaces/login.interface";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-constructor(private http: HttpClient){}
+  private _currentUser = new BehaviorSubject(null);
 
-login(value: ILogin) {
- // @ts-ignore
-  return this.http.post('https://mentors-portal.herokuapp.com/auth/login', {
-    username: value.username,
-    password: value.password
-  } )
+  constructor(private http: HttpClient) {
+  }
 
-}
-registration(value: ILogin) {
-  return this.http.post('https://mentors-portal.herokuapp.com/auth/registration', {
-    username: value.username,
-    password: value.password
-  })
-}
-getUsers(value: string) {
-  return this.http.get('https://mentors-portal.herokuapp.com/auth/users', {
-    headers: {
-      Authorization: `Bearer ${value}`
-    }
-  })
-}
+  login(value: ILogin) {
+    // @ts-ignore
+    return this.http.post('https://mentors-portal.herokuapp.com/auth/login', {
+      username: value.username,
+      password: value.password
+    })
+
+  }
+
+  registration(value: ILogin) {
+    return this.http.post('https://mentors-portal.herokuapp.com/auth/registration', {
+      username: value.username,
+      password: value.password
+    })
+  }
+
+  getUsers(value: string) {
+    return this.http.get('https://mentors-portal.herokuapp.com/auth/users', {
+      headers: {
+        Authorization: `Bearer ${value}`
+      }
+    })
+  }
+
+  get currentUser() {
+    return this._currentUser.getValue()
+  }
+
+  set currentUser(value) {
+    this._currentUser.next(value)
+  }
+
 }
