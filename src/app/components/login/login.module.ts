7f2dee2e2252f9a3d1c6cc5139b8c900032aa491
Ihor Chernysh <102924490+IckCrsh@users.noreply.github.com>
2022-08-22 16:34:52 +0300

    created spinner and new getUser method

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoginComponent} from "./login.component";
 import {LoginRoutingModule} from "./login-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { ProgressSpinnerModule } from "primeng/progressspinner";
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        LoginRoutingModule,
+        ReactiveFormsModule,
+        ProgressSpinnerModule
+    ]
 })
 export class LoginModule { }
